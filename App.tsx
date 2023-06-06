d961585832cf8594c9d479a68dc31c0fbb4c16c7
Fikry C. Farenza <vickyfarenza@gmail.com>
2023-06-06 09:40:04 +0700

    Fixed double action bar :ambulance:

--- App.tsx
+++ App.tsx
@@ -2,14 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import AppContainer from "./src/navigation/AppContainer";
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import {app} from "./firebase";
 
 export default function App() {
   return (
       <SafeAreaProvider>
           <StatusBar style="auto"/>
-          <SafeAreaView style={styles.container}>
-              <AppContainer/>
-          </SafeAreaView>
+          <AppContainer/>
       </SafeAreaProvider>
   );
 }
